0967f23ede08d2a891ca9c1fd4c1f7903d36498a
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-04-23 00:03:06 +0900

    code spliting & emotion

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,19 +1,19 @@
-import React, { FC } from 'react';
-import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
-import Login from '@pages/LogIn';
-import Signup from '@pages/SignUp';
+import React, { FC } from "react";
+import loadable from "@loadable/component";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+const Login = loadable(() => import("@pages/LogIn"));
+const Signup = loadable(() => import("@pages/SignUp"));
 
 const App: FC = () => {
-
-    return (
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Navigate replace to='login' />}/>
-        <Route path='/login' element={ <Login /> } />
-        <Route path='/signup' element={ <Signup /> } />
+        <Route path="/" element={<Navigate replace to="login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
-    );
+  );
 };
 
 export default App;
--- pages/LogIn/index.tsx
+++ pages/LogIn/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const LogIn = () => {
-    return (
-        <div>로그인</div>
-    );
+  return <div>로그인</div>;
 };
 
 export default LogIn;
