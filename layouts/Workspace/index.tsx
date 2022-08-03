85c0c0c4bfb6a5fd3e0450e9bb156450f8466dda
dongho YUN <76724739+dhboys@users.noreply.github.com>
2022-08-03 22:59:43 +0900

    :sparkles: workspace 추가 && typings 추가

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -11,6 +11,8 @@ import {
   MenuScroll,
   ProfileModal,
   LogOutButton,
+  WorkspaceButton,
+  AddButton,
 } from "@layouts/Workspace/styles";
 import axios from "axios";
 import useSWR from "swr";
@@ -19,12 +21,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import loadable from "@loadable/component";
 import gravatar from "gravatar";
 import Menu from "@components/Menu";
+import { Link } from "react-router-dom";
+import { IUser } from "@typings/db";
 
 const Channel = loadable(() => import("@pages/Channel"));
 const DirectMessage = loadable(() => import("@pages/DirectMessage"));
 
 const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
-  const { data, error, mutate } = useSWR("http://localhost:3095/api/users", fetcher, {
+  const { data: userData, error, mutate } = useSWR<IUser | false>("http://localhost:3095/api/users", fetcher, {
     dedupingInterval: 1000, // 지정된 시간안에서는 같은 key를 서버에 호출하지 않고 caching된 데이터 사용
   });
 
@@ -47,7 +51,11 @@ const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
     setShowUserMenu((prev) => !prev);
   }, [showUserMenu]);
 
-  if (!data) {
+  const onClickCreateWorkspace = useCallback(() => {
+
+  }, [])
+
+  if (!userData) {
     return <Navigate replace to="/login" />;
   }
 
@@ -56,24 +64,31 @@ const Workspace: FC<React.PropsWithChildren<{}>> = ({ children }) => {
       <Header>
         <RightMenu>
           <span onClick={onClickUserProfile}>
-            <ProfileImg src={gravatar.url(data?.email, { s: "28px", d: "retro" })} alt={data?.email} />
+            <ProfileImg src={gravatar.url(userData?.email, { s: "28px", d: "retro" })} alt={userData?.email} />
             {showUserMenu && 
             (<Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
               <ProfileModal>
-                <img src={gravatar.url(data?.email, { s: "28px", d: "retro" })} alt={data?.email} />
+                <img src={gravatar.url(userData?.email, { s: "28px", d: "retro" })} alt={userData?.email} />
                 <div>
-                  <span id="profile-name">{data?.nickname}</span>
+                  <span id="profile-name">{userData?.nickname}</span>
                   <span id="profile-active">Active</span>
                 </div>
                 <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
               </ProfileModal>
-              
               </Menu>)}
           </span>
         </RightMenu>
       </Header>
       <WorkspaceWrapper>
-        <Workspaces>test</Workspaces>
+        <Workspaces>{userData?.Workspaces?.map((workspace) => {
+          return (
+            <Link key={workspace?.id} to={`/workspace/${123}/channel/일반`}>
+              <WorkspaceButton>{workspace?.name?.slice(0,1).toUpperCase()}</WorkspaceButton>
+            </Link>
+          );
+        })}
+        <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
+        </Workspaces>
         <Channels>
           <WorkspaceName>Sleact</WorkspaceName>
           <MenuScroll>menu scroll</MenuScroll>
